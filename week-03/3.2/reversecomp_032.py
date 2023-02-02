063182fc03e9f0e9bc29c5ff655a09161704da17
Ali-thepro <aliahmad2005yt@gmail.com>
2023-02-02 15:11:29 +0000

    changes

--- week-03/3.2/reversecomp_032.py
+++ week-03/3.2/reversecomp_032.py
@@ -29,16 +29,10 @@ def binsearch(query, sorted_list):
     return False
 
 
-# Build the list of words
 lines = [line.strip() for line in sys.stdin]
 
-# Build the sorted list of words (don't have to sort the list)
 sorted_list = sorted([w.lower() for w in lines])
 
-# Words whose reverse is in the list (binsearch)
 reverse = [w for w in lines if len(w) >= 5 and binsearch(w[::-1].lower(), sorted_list)]
-
-# Word whose reverse is in the list (brute-force search is too slow)
-# reverse = [w for w in l if len(w) >= 5 and w[::-1].lower() in sl]
 
 print(reverse)
