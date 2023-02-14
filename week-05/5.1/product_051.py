a962eb579fcd4934774afaf4e7ad104a6912ef77
Ali-thepro <aliahmad2005yt@gmail.com>
2023-02-14 17:30:52 +0000

    changes

--- week-05/5.1/product_051.py
+++ week-05/5.1/product_051.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
 
 import sys
-
 n = sys.stdin.readline().strip()
 
 while int(n) > 9:
-    n = str(n)
-    nums = [int(i) for i in n]
     product = 1
-    for i in nums:
-        if i != 0:
-            product *= i
+    for i in str(n):
+        if i != "0":
+            product *= int(i)
     n = product
 print(n)
