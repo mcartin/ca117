dc6be29e04518c7c7e4bf0cb157574ce7cb313db
Ali-thepro <aliahmad2005yt@gmail.com>
2023-02-18 15:58:42 +0000

    changes

--- week-03/3.1/repcomps_031.py
+++ week-03/3.1/repcomps_031.py
@@ -5,7 +5,8 @@ import sys
 for line in sys.stdin:
     nums = int(line)
     r = range(1, nums + 1)
-    output = [0 if n % 3 else n for n in r]
+    output = [n if not n % 3 else 0 for n in r]
+    # output = [0 if n % 3 else n for n in r]
     # output = [0 if not n % 3 == 0 else n for n in r]
     # output = [0 if not n % 3 != 0 else n for n in r]
     print(f"Non-multiples of 3 replaced: {output}")
