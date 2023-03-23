2ed6a50d7524b61f944d74d01ad78d7575a0e794
Ali-thepro <aliahmad2005yt@gmail.com>
2023-03-23 15:02:28 +0000

    10.2

--- week-10/10.2/maximum_102.py
+++ week-10/10.2/maximum_102.py
@@ -1,8 +1,8 @@
-def maximum(list):
-    if len(list) == 1:
-        return list[0]
+def maximum(l):
+    if len(l) == 1:
+        return l[0]
     else:
-        if list[0] > maximum(list[1:]):
-            return list[0]
+        if l[0] > maximum(l[1:]):
+            return l[0]
         else:
-            return maximum(list[1:])
+            return maximum(l[1:])
